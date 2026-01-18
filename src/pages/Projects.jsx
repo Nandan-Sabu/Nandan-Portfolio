3f58f86e66b8df3615f061f09b533a1ffa1485e7
Nandan Sabu <108638088+Nandan-Sabu@users.noreply.github.com>
2026-01-18 23:41:29 +1300

    content and projects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -14,6 +14,15 @@ import project2_3 from "../project_pics/platformer_game_img3.png";
 import project2_4 from "../project_pics/platformer_game_img4.png";
 
 import project3_1 from "../project_pics/machine_language_img.png";
+import project3_2 from "../project_pics/machine_language_img2.png";
+
+import project4_1 from "../project_pics/bouncing_shape_img.png";
+import project4_2 from "../project_pics/bouncing_shape_img2.png";
+import project4_3 from "../project_pics/bouncing_shape_img3.png";
+import project4_4 from "../project_pics/bouncing_shape_img4.png";
+import project4_5 from "../project_pics/bouncing_shape_img5.png";
+import project4_6 from "../project_pics/bouncing_shape_img6.png";
+import project4_7 from "../project_pics/bouncing_shape_img7.png";
 
 const Projects = () => {
   const [activeProject, setActiveProject] = useState(null);
@@ -22,7 +31,15 @@ const Projects = () => {
     {
       title: "Animal Re-identification Desktop Application",
       description: `
-        Desktop application that implements AI to 
+        Developed a cross-platform desktop application that uses artificial intelligence to identify 
+        and re-identify animals from images captured by trap cameras. The system assigns each detected 
+        animal their own digital identity, enabling the tracking of population counts and migration 
+        patterns across specific locations. The application is designed to support pest management 
+        efforts in New Zealand, particularly for monitoring species such as stoats.
+
+        The frontend was built using React and deployed as a desktop application via Electron. A 
+        Node.js and Express.js backend handles application logic and data flow, while an AI inference 
+        server built with Anaconda and PyTorch performs animal detection and re-identification.
       `,
       images: [
         project1_1,
@@ -35,9 +52,12 @@ const Projects = () => {
     {
       title: "Multi-Level Platformer Game",
       description: `
-        A cross-platform desktop application developed using Electron.
-        The project involved designing the UI, implementing core logic,
-        and integrating local data storage.
+        Developed a multi-level 2D platformer game using Python, applying object-oriented 
+        programming principles to structure core gameplay systems. The game features multiple 
+        interconnected levels designed with Tiled, incorporating player movement, collision
+         handling, enemies, and interactive environments. Gameplay mechanics include collectible
+          items, moving platforms, portals, hazards, and level transitions, supported by animations 
+          and audio to enhance the player experience.
       `,
       images: [
         project2_1,
@@ -50,21 +70,43 @@ const Projects = () => {
     {
       title: "Machine Learning Model for Predictive Analytics",
       description: `
-        Developed a machine learning model using Python. This model uses training data
+        Developed a machine learning model using Python and Jupyter. This model uses training data
         to develop a decision tree which is be calculating the entropy and information gain 
         to find the best split. this tree can then be used on testing data, which is a new set of data, and 
         predict outcomes based on patterns learned from the training data. The data that is given will
         be for different features and the model will predict the target variable based on these 
         given features. For example, if you wanted to know if a person earns more or less than 50k annually, 
         marital_status being "Married-civ-spouse" would be the best split to determine this outcome and 
-        each test instance is evaluated by traversing the tree according to its feature values until a leaf node is reached
+        each test instance is evaluated by traversing the tree according to its feature values until a leaf 
+        node is reached
         .
 
       `,
       images: [
         project3_1,
-        project3_1,
-        project3_1,
+        project3_2,
+      ],
+    },
+
+    {
+      title: "Bouncing shapes Application",
+      description: `
+        Developed a Java-based desktop application with a graphical user interface using object-oriented 
+        programming principles. The application allows users to dynamically create and manage shapes, 
+        such as squares and rectangles, which are added to a hierarchical (nested) node structure. Each 
+        shape is animated to bounce within the application window, and users can further insert nested 
+        shapes inside existing shapes, demonstrating recursive composition and inheritance. The project 
+        was implemented using Java in Notepad++, emphasizing core OOP concepts including encapsulation, 
+        polymorphism, and class hierarchies, as well as event handling and graphical rendering.
+      `,
+      images: [
+        project4_1,
+        project4_2,
+        project4_3,
+        project4_4,
+        project4_5,
+        project4_6,
+        project4_7,
       ],
     },
   ];
